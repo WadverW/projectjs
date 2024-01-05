7ff46f962da473bbf1ea598a310f3f4aee5025b0
Wadver <wadverw@gmail.com>
2024-01-05 12:06:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// const store_name = `redneck`;
+// const storeDescription = {
+//     budget: 100000,
+//     employees: [`david`, `axe`, `vadim`],
+//     products: {
+//         milk: 55,
+//         beer: 37
+//     },
+//     open: true
+// }
+
+// const result = confirm(`Are you sure?`);
+// console.log(result); //дает тру в консоли
+
+// const answer = prompt(`Вам исполнилось 18?`, ``);
+// console.log(answer);
+
+// const answer = prompt(`Вам исполнилось 18?`, ``);
+// console.log(typeof(answer));
+
+//вся инф от пользователя приходит в виде строк!!!!!!!!!!
+
+// const answer = +prompt(`Вам исполнилось 18?`, ``);
+// console.log(typeof(answer));
+
+// const answer = +prompt(`Вам исполнилось 18?`, ``);
+// console.log(answer + 5);
+
+// document.write(`Hello!`);
+
+// const answer=[];
+// answer[0]=prompt(`Your name?`, ``);
+// answer[1]=prompt(`Your surename?`, ``);
+// answer[2]=prompt(`Your age?`, ``);
+// console.log(answer);
+// document.write(answer);
+// console.log(typeof(answer));
+
+// const catagory =`toys`;
+// console.log(`http://someurl.com/` + catagory);
+// //or 
+// console.log(`http://someurl.com/${catagory}`); //С ДВ КОВ НЕ РАБОТАЕТ
+
+// const user=`wadver`;
+// alert(`Hello, ${user}`);
+
+// const isChecked=true,
+//     isClosed=false;
+// console.log(isChecked || isClosed);
+
+// const isChecked=true,
+//     isClosed=false;
+// console.log(isChecked && !isClosed);
+
+console.log(2+2*5 !=10); //true
